080526d867219387b8f138ccfb2f93d0c37b162a
CrankyNerd <brupp24@gmail.com>
2026-09-07 11:43:44 -0400

    Initial Commit

---
+++ StreamCue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreamCue
+//
+//  Created by Bryan Rupp on 9/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreamCue/StreamCueApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamCueApp.swift
+//  StreamCue
+//
+//  Created by Bryan Rupp on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct StreamCueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
